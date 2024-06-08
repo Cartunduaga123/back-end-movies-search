18ed3a4940c10efa42cba52ebeed146d25ab7535
Javier Alejandro Castañeda Celis <desarrollo@bitu.com.co>
2024-06-08 13:23:51 -0500

    config

---
+++ src/main/java/com/unir/movie_app_search/config/BeanConfig.java
@@ -0,0 +1,14 @@
+package com.unir.movie_app_search.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
