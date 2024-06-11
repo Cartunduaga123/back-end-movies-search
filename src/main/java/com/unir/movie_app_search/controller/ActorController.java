2f870763c147e88bf300e5130f9382011eb6e214
Javier Alejandro Castañeda Celis <desarrollo@bitu.com.co>
2024-06-11 15:09:38 -0500

    Documentación swagger rutas

--- src/main/java/com/unir/movie_app_search/controller/ActorController.java
+++ src/main/java/com/unir/movie_app_search/controller/ActorController.java
@@ -1,6 +1,7 @@
 package com.unir.movie_app_search.controller;
 
 import com.unir.movie_app_search.persistence.entity.ActorEntity;
+import com.unir.movie_app_search.persistence.entity.MovieEntity;
 import com.unir.movie_app_search.service.ActorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -47,6 +48,25 @@ public class ActorController {
     }
 
     @PostMapping("/actores")
+    @Operation(
+            operationId = "Insertar un actor",
+            description = "Operacion de escritura",
+            summary = "Se crea un actor a partir de sus datos.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Datos del actor a crear.",
+                    required = true,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = MovieEntity.class))))
+    @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MovieEntity.class)))
+    @ApiResponse(
+            responseCode = "400",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Datos incorrectos introducidos.")
+    @ApiResponse(
+            responseCode = "404",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "No se ha encontrado el actor con el identificador indicado.")
     public ResponseEntity<ActorEntity> add(@RequestBody ActorEntity movie) {
         if (movie.getId() == null || !this.actorService.exists(movie.getId())) {
             return ResponseEntity.ok(this.actorService.save(movie));
@@ -55,6 +75,21 @@ public class ActorController {
     }
 
     @PutMapping("/actores/{id}")
+    @Operation(
+            operationId = "Modificar totalmente un actor",
+            description = "Operacion de escritura",
+            summary = "Se modifica totalmente un actor.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Datos del actor a actualizar.",
+                    required = true,
+                    content = @Content(mediaType = "application/merge-patch+json", schema = @Schema(implementation = MovieEntity.class))))
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MovieEntity.class)))
+    @ApiResponse(
+            responseCode = "404",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Actor no encontrada.")
     public ResponseEntity<ActorEntity> update(@PathVariable Integer id, @RequestBody ActorEntity movie) {
         if (id != null && actorService.exists(id)) {
             movie.setId(id);
@@ -64,6 +99,17 @@ public class ActorController {
     }
 
     @DeleteMapping("/actores/{idActor}")
+    @Operation(
+            operationId = "Eliminar un actor",
+            description = "Operacion de escritura",
+            summary = "Se elimina una actor a partir de su identificador.")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)))
+    @ApiResponse(
+            responseCode = "404",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "No se ha encontrado la actor con el identificador indicado.")
     public ResponseEntity<Void> delete(@PathVariable int idActor) {
         if (this.actorService.exists(idActor)) {
             this.actorService.delete(idActor);
--- src/main/java/com/unir/movie_app_search/controller/CategoryController.java
+++ src/main/java/com/unir/movie_app_search/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.unir.movie_app_search.controller;
 
 import com.unir.movie_app_search.persistence.entity.CategoryEntity;
+import com.unir.movie_app_search.persistence.entity.MovieEntity;
 import com.unir.movie_app_search.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -48,6 +49,25 @@ public class CategoryController {
     }
 
     @PostMapping("/categorias")
+    @Operation(
+            operationId = "Insertar una categoria",
+            description = "Operacion de escritura",
+            summary = "Se crea una categoria a partir de sus datos.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Datos de la categoria a crear.",
+                    required = true,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = MovieEntity.class))))
+    @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MovieEntity.class)))
+    @ApiResponse(
+            responseCode = "400",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Datos incorrectos introducidos.")
+    @ApiResponse(
+            responseCode = "404",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "No se ha encontrado la categoria con el identificador indicado.")
     public ResponseEntity<CategoryEntity> add(@RequestBody CategoryEntity movie) {
         if (movie.getId() == null || !this.categoryService.exists(movie.getId())) {
             return ResponseEntity.ok(this.categoryService.save(movie));
@@ -56,6 +76,21 @@ public class CategoryController {
     }
 
     @PutMapping("/categorias/{id}")
+    @Operation(
+            operationId = "Modificar totalmente una categoria",
+            description = "Operacion de escritura",
+            summary = "Se modifica totalmente una categoria.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Datos de la categoria a actualizar.",
+                    required = true,
+                    content = @Content(mediaType = "application/merge-patch+json", schema = @Schema(implementation = MovieEntity.class))))
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MovieEntity.class)))
+    @ApiResponse(
+            responseCode = "404",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "Categoria no encontrada.")
     public ResponseEntity<CategoryEntity> update(@PathVariable Integer id, @RequestBody CategoryEntity movie) {
         if (id != null && categoryService.exists(id)) {
             movie.setId(id);
@@ -65,6 +100,17 @@ public class CategoryController {
     }
 
     @DeleteMapping("/categorias/{idCategory}")
+    @Operation(
+            operationId = "Eliminar una categoria",
+            description = "Operacion de escritura",
+            summary = "Se elimina una categoria a partir de su identificador.")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)))
+    @ApiResponse(
+            responseCode = "404",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class)),
+            description = "No se ha encontrado la categoria con el identificador indicado.")
     public ResponseEntity<Void> delete(@PathVariable int idCategory) {
         if (this.categoryService.exists(idCategory)) {
             this.categoryService.delete(idCategory);
